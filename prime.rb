62875c8ecf5bd79bfaaab2cf979f4514476aef58
Davidlago104 <davidlago104@gmail.com>
2018-06-25 01:22:20 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,12 +1,12 @@
 # Add  code here!
 def prime?(integer)
 if integer <= 1 
-  false 
+  return false 
 end
 i = 2 
 while i < integer
   if integer % i == 0 
-    false 
+    return false 
   end
   i += 1 
 end
